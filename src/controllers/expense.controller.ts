1a137995ec50ad73b06abb69e8e48b21f3105814
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-10 13:35:04 +0100

    Add: update expense and delete expense

--- src/controllers/expense.controller.ts
+++ src/controllers/expense.controller.ts
@@ -18,8 +18,27 @@ export const createExpense: RequestHandler = async (req, res, next) => {
 
 export const updateExpense: RequestHandler = async (req, res, next) => {
 
+  const expense = await expenseModel.findById(req.body.id)
+
+  if (!expense) {
+    res.status(400).json({ message: "This expense doesn't exist" })
+  }
+
+  const updatedExpense = await expenseModel.findByIdAndUpdate(expense, req.body, { new: true })
+
+  res.status(201).json({ message: "Expense updated.", expense: expense })
 }
 
 export const deleteExpense: RequestHandler = async (req, res, next) => {
+
+  const expense = await expenseModel.findById(req.body.id)
+
+  if (!expense) {
+    res.status(400).json({ message: "This expense doesn't exist" })
+  }
+
+  await expenseModel.findByIdAndDelete(expense)
+
+  res.status(200).json({ message: `Expense with id ${req.params.id} has been deleted.` })
 
 }
