dfc7a2d214dc021f0ab9e0b021e45dd4f0ed851e
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-10 15:33:44 +0100

    Using cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 import expenseRouter from './routes/expense.routes'
 import connectDB from './config/db'
+import cors from 'cors'
 dotenv.config()
 
 connectDB()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
