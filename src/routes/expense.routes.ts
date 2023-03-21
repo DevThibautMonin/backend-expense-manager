321599ce3391442963db318ca86b80d6f0e56850
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-21 11:00:13 +0100

    add: expenses by user

--- src/routes/expense.routes.ts
+++ src/routes/expense.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { createExpense, deleteExpense, getExpenses, updateExpense } from '../controllers/expense.controller'
+import { createExpense, deleteExpense, getExpenses, updateExpense, getExpensesByUser } from '../controllers/expense.controller'
 import { verifyUserToken } from '../middlewares/authentication.middleware'
 
 const expenseRouter = Router()
 
 expenseRouter.post('/', createExpense)
 expenseRouter.get('/', verifyUserToken, getExpenses)
+expenseRouter.get('/:userId', verifyUserToken, getExpensesByUser)
 expenseRouter.put('/:id', updateExpense)
 expenseRouter.delete('/:id', deleteExpense)
 
